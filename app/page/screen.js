f961a62f986151ac72e998fa83844d875ca145a7
dimaswirawan <dimas_jumbo@yahoo.com>
2021-10-29 11:22:47 +0700

    init

--- app/page/screen.js
+++ app/page/screen.js
@@ -10,7 +10,7 @@ function screen(res) {
 		konten += `<div class="main-screen p-3">${image} <br> <br><span class="tombol-mulai" onclick="screenKondisional({parent:0,id:'${id}'})">Mulai</span></div>`;
 	}else if (id == 3) {
 		image = `<img src="${image_home_blob}" alt="">`;
-		konten += `<div class="p-3">${image}</div><div class="p-3"><p>Lorem ipsum, dolor sit amet consectetur, adipisicing elit. Facilis explicabo, harum, officiis corporis labore sunt perspiciatis dignissimos aliquid saepe, totam nihil vitae, cumque? Voluptate, placeat impedit mollitia ab magnam qui.</p></div><span class="icon-tanya text-white"></span>`;
+		konten += `<div class="p-3">${image}</div><div class="p-3"><p>Keberhasilan menyusui sangat dipengaruhi oleh pengetahuan Ibu, dukungan menyusui dari provider kesehatan serta keluarga. AplikASI Menyusui siap membersamai Ibu dalam menghadapi hambatan dan permasalahan yang seringkali di jumpai dalam proses menyusui. Menyusui menjadi lebih Mudah dan Menyenangkan.</p></div><span class="icon-tanya text-white"></span>`;
 	}else if (id == 4) {
 		image = `<img src="${image_ayah_blob}" alt="">`;
 		konten += `<div class="main-screen p-3">${image} <br> <br><span class="tombol-mulai" onclick="screenKondisional({parent:0,id:'${id}'})">Mulai</span></div>`;
